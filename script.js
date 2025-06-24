4c988fdf933a6463c99c4600a53abe14b344bcdd
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-06-25 04:34:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 window.onload = () => {
-  const map = L.map('map').setView([-1.64, 101.21], 12); // Titik tengah peta
+  const map = L.map('map').setView([-1.64, 101.21], 12); // Lubuk Gadang Selatan
 
-  // Basemap OpenStreetMap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+    attribution: '© OpenStreetMap'
   }).addTo(map);
 
-  // Tile layer dari Google Earth Engine
   const geeTileUrl = 'https://earthengine.googleapis.com/v1/projects/ee-mrgridhoarazzak/maps/7ee0dbe3066bdcfa7c227c635f347084-a6496335255e886b362e38bc1a1364ed/tiles/{z}/{x}/{y}';
 
   L.tileLayer(geeTileUrl, {
